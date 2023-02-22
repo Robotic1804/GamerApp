a6f4d377394e2fac367c3fc920be3e4736f32562
Norman Navarro <norman18navarro@hotmail.com>
2023-02-21 22:25:09 -0600

    Sidebar: json web Token

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from '../services/modal.service';
+import { AuthService } from '../services/auth.service';
+import {AngularFireAuth} from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +9,11 @@ import { ModalService } from '../services/modal.service';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-  constructor(public modal: ModalService) {
+  
+
+  constructor(public modal:ModalService,
+    public auth: AuthService,
+    private afAuth: AngularFireAuth) {
     
   }
 
@@ -15,6 +21,11 @@ export class NavComponent {
     
     $event.preventDefault()
 this.modal.toggleModal('auth')
+  }
+
+ async logout($event: Event) {
+    $event.preventDefault()
+  await  this.afAuth.signOut()
   }
 
 }
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-login',
@@ -11,12 +12,37 @@ export class LoginComponent {
     emails: '',
     password: '',
     
-
   }
 
-  login() {
-    
-    console.log(this.credentials)
+  showAlert = false
+  alertMsg = 'Please wait! We are loggin you in.'
+  alertColor = 'blue'
+  inSubmission = false
+
+  constructor(private auth: AngularFireAuth){}
+
+  async login() {
+    this.showAlert = true
+    this.alertMsg = 'Please wait! We are loggin you in.'
+    this.alertColor = 'blue'
+    this.inSubmission = true
+   try {
+  await   this.auth.signInWithEmailAndPassword(
+        this.credentials.emails, this.credentials.password
+      )
+   }
+   catch (e) {
+     this.inSubmission = false
+     this.alertMsg = 'An unexpected error ocurred. Please try again later.'
+     this.alertColor = 'red'
+     
+     console.log(e)
+
+     return
+     
+   }
+    this.alertMsg = 'Sucess! You are now logged in'
+    this.alertColor = 'green'
   }
 
 }
