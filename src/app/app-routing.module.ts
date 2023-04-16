ceed339088e0c77e762100e4104659ddbab3fcab
Norman Navarro <norman18navarro@hotmail.com>
2023-04-15 19:42:52 -0600

    videojs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ClipComponent } from './clip/clip.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { ClipService } from './services/clip.service';
 
 
 
@@ -13,11 +14,25 @@ const routes: Routes = [
     component: HomeComponent,
    
   },
+
   { path: 'about', component: AboutComponent },
+  
   {
     path: 'clip/:id',
-    component: ClipComponent
+    component: ClipComponent,
+    resolve: {
+      clip: ClipService
+    }
+
   },
+
+  {
+    path:'',//dashboard//manage, dashboard/upload
+
+    loadChildren: async () => (await import('./video/video.module')).VideoModule
+  },
+
+
   {
     path: '**',
     component: NotFoundComponent
--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -85,4 +85,20 @@ export class ManageComponent implements OnInit {
     })
   }
 
+  async copyToClipboard($event: MouseEvent, docID: string | undefined) {
+    
+    $event.preventDefault()
+
+
+    if(!docID) {
+    
+
+    }
+    
+    const url = `${location.origin}/clip/${docID}`
+    await navigator.clipboard.writeText(url)
+
+    alert('Link Copied!')
+  }
+
 }
