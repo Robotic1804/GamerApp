2ee8135d05091f5976a95f9b608eb12c86fee95b
Norman Navarro <93636275+Robotic1804@users.noreply.github.com>
2025-10-13 14:25:42 -0700

    Fix: Add environment files for Vercel build

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+
+  firebase: {
+    apiKey: 'AIzaSyBsozcKvOtPilPsn_m92TCJeaZ4c6ktIcc',
+    authDomain: 'clips5.firebaseapp.com',
+    projectId: 'clips5',
+    storageBucket: 'clips5.appspot.com',
+    messagingSenderId: '925593119925',
+    appId: '1:925593119925:web:f82528e96a3da714e022fc',
+  },
+};
